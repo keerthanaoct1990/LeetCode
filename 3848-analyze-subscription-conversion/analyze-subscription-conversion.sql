84389da2c44425ebc8f72194d8904eea520a8f37
Keerthana Sasikumar <61818477+keerthanaoct1990@users.noreply.github.com>
2025-05-26 15:40:37 -0400

    Time: 300 ms (82.19%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
-SELECT user_id, 
+with subs_cte as
+(SELECT user_id, 
     ROUND(AVG(CASE WHEN activity_type = 'free_trial' then activity_duration ELSE NULL END), 2)AS trial_avg_duration,
     ROUND(AVG(CASE WHEN activity_type = 'paid' then activity_duration ELSE NULL END), 2)AS paid_avg_duration
 FROM UserActivity
-group by user_id
-having paid_avg_duration is not null
+group by user_id)
+
+select * from subs_cte 
+where paid_avg_duration is not null
 order by user_id
