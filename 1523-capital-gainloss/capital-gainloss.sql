d78f435736f43d29540c7560147ebba7feb8469e
Keerthana Sasikumar <61818477+keerthanaoct1990@users.noreply.github.com>
2025-05-26 12:19:57 -0400

    Time: 507 ms (67.07%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -2,7 +2,6 @@
 select stock_name,
     sum(case when operation = 'Buy' then -price 
     when operation = 'Sell' then price 
-    else 0 
     end) as capital_gain_loss
 from stocks
 group by stock_name;
