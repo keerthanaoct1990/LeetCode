6816819f8d0cddc6a18564a33d4702739ffedc9b
Keerthana Sasikumar <61818477+keerthanaoct1990@users.noreply.github.com>
2026-02-14 14:41:10 -0500

    Time: 459 ms (12.71%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,37 +1,13 @@
-WITH ride_data AS (
-    SELECT t.request_at, t.status
-    FROM Trips t
-    JOIN Users c 
-        ON t.client_id = c.users_id AND c.banned = 'No'
-    JOIN Users d 
-        ON t.driver_id = d.users_id AND d.banned = 'No'
-    WHERE t.request_at BETWEEN '2013-01-19' AND '2013-10-03'
-),
-
-total_requests AS (
-    SELECT 
-        request_at,
-        COUNT(*) AS total_req
-    FROM ride_data
-    GROUP BY request_at
-),
-
-total_cancels AS (
-    SELECT 
-        request_at,
-        COUNT(*) AS total_cancel
-    FROM ride_data
-    WHERE status IN ('cancelled_by_driver', 'cancelled_by_client')
-    GROUP BY request_at
-)
-
 SELECT 
-    tr.request_at AS Day,
-    ROUND(
-        COALESCE(tc.total_cancel, 0) * 1.0 / tr.total_req,
-        2
-    ) AS "Cancellation Rate"
-FROM total_requests tr
-LEFT JOIN total_cancels tc
-    ON tr.request_at = tc.request_at
-ORDER BY tr.request_at;
+    request_at AS Day
+    ,ROUND(SUM(CASE WHEN status='cancelled_by_driver' OR status='cancelled_by_client' THEN 1.0 ELSE 0 END) / COUNT(request_at),2) 
+    AS "Cancellation Rate" /*use quote as in output we need to show like this*/
+FROM  Trips t
+JOIN Users c
+    ON t.client_id=c.users_id /*include clients who are banned*/
+    AND c.banned='No'
+JOIN Users u
+    ON t.driver_id=u.users_id/*include drivers who are banned*/
+    AND u.banned='No'
+WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at
