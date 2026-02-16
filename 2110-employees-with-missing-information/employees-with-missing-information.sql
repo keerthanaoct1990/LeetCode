e9480e305be33dbe10f2f5ae6c041b923030b96a
Keerthana Sasikumar <61818477+keerthanaoct1990@users.noreply.github.com>
2026-02-16 17:28:06 -0500

    Time: 380 ms (46.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT e.employee_id FROM Employees e
+LEFT JOIN Salaries s
+ON e.employee_id = s.employee_id
+WHERE name IS NULL OR salary IS NULL
+UNION 
+SELECT s.employee_id FROM Salaries s
+LEFT JOIN Employees e
+ON s.employee_id = e.employee_id
+WHERE name IS NULL OR salary IS NULL
