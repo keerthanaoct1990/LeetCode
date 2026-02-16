a80662e210405fad7b12482dc107090a93f727f6
Keerthana Sasikumar <61818477+keerthanaoct1990@users.noreply.github.com>
2026-02-15 22:12:30 -0500

    Time: 372 ms (53.88%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
-select(select distinct salary from Employee
-order by salary desc
-limit 1
-offset 1) as SecondHighestSalary
+/* Write your T-SQL query statement below */
+with cte as 
+(select id, salary ,
+dense_rank() over(order by salary desc) as rn
+from Employee)
+select max(salary) as SecondHighestSalary  from cte where rn = 2
