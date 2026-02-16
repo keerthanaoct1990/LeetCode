0b1fa9160abb8f8fa483a45f642dad1d43ffa388
Keerthana Sasikumar <61818477+keerthanaoct1990@users.noreply.github.com>
2026-02-16 17:17:10 -0500

    Time: 801 ms (83.29%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+WITH cte AS(
+    SELECT user_id, time_stamp,
+ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY time_stamp DESC) AS rn
+FROM Logins 
+WHERE YEAR(time_stamp)=2020)
+SELECT user_id, time_stamp AS last_stamp
+FROM cte WHERE rn = 1
+
