2429568d2f9eb469762aafde6430f315a040d27b
Keerthana Sasikumar <61818477+keerthanaoct1990@users.noreply.github.com>
2026-03-06 13:50:00 -0500

    Time: 413 ms (48.82%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,12 +1,13 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
-  set N = N-1;
-  RETURN (
-      # Write your MySQL query statement below.
-        select distinct salary
-        from Employee
-        order by salary desc
-        limit 1
-        offset N
-  );
+    RETURN (
+        /* This subquery finds all unique ranks */
+        SELECT TOP 1 salary
+        FROM (
+            SELECT salary, 
+                   DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+            FROM Employee
+        ) AS SubQuery
+        WHERE rnk = @N
+    );
 END
